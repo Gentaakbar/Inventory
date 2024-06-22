59205d0bef8d2413006ce16154d4f3244b02ba2f
Gentaakbar <gentalol39@gmail.com>
2024-06-22 11:56:21 +0700

    menambahkan db

--- dbinventory.sql
+++ dbinventory.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Feb 2023 pada 08.49
--- Versi server: 10.4.22-MariaDB
--- Versi PHP: 8.0.13
+-- Waktu pembuatan: 22 Jun 2024 pada 06.54
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -86,6 +86,13 @@ CREATE TABLE `barangmasuk` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data untuk tabel `barangmasuk`
+--
+
+INSERT INTO `barangmasuk` (`id`, `idtransaksi`, `tanggal`, `lokasi`, `namasupplier`, `alamat`, `telepon`, `kodebarang`, `namabarang`, `satuan`, `jumlah`) VALUES
+(131, 1700001, '2024-06-04', 'Gudang', 'nisin', 'jakar', '123131', 1, 'sabun', 'gr', 1);
+
+--
 -- Trigger `barangmasuk`
 --
 DELIMITER $$
@@ -122,6 +129,13 @@ CREATE TABLE `databarang` (
   `jumlah` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `databarang`
+--
+
+INSERT INTO `databarang` (`id`, `kodebarang`, `namabarang`, `satuan`, `jumlah`) VALUES
+(68, 1, 'sabun', 'gr', 13);
+
 -- --------------------------------------------------------
 
 --
@@ -135,6 +149,14 @@ CREATE TABLE `datacustomer` (
   `telepon` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `datacustomer`
+--
+
+INSERT INTO `datacustomer` (`id`, `namacustomer`, `alamat`, `telepon`) VALUES
+(11, 'genta', 'jakarta', '1423425252'),
+(12, 'jadil', 'rumbut', '12141241');
+
 -- --------------------------------------------------------
 
 --
@@ -147,6 +169,13 @@ CREATE TABLE `datasupplier` (
   `alamat` varchar(200) NOT NULL,
   `telepon` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `datasupplier`
+--
+
+INSERT INTO `datasupplier` (`id`, `namasupplier`, `alamat`, `telepon`) VALUES
+(7, 'nisin', 'jakarta', '123131');
 
 -- --------------------------------------------------------
 
@@ -171,9 +200,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`, `role`, `avatar`, `created_at`, `last_login`) VALUES
-(1, '', 'yosuak7@gmail.com', 'Admin', '$2y$10$r6ywVQmzGZQKW5pIabgoC.vyyABceBlTb.wa72MkAKl0vpPeUiaJ.', 1, NULL, '2022-05-20 16:27:14', '0000-00-00 00:00:00'),
-(6152, '', 'eka1@gmail.com', 'eka12', '$2y$10$igkG68nfjHVy.MbL1kR3CecGXDOhvC5wg7NTOGbCpODfoVi1ILwf.', 1, NULL, '2022-11-21 04:16:03', NULL),
-(6156, '', 'yosuak@gmail.com', 'yosua', '$2y$10$rw3j1Rv2uo0MXC5I6sKzVunysh7Qe5pKIBZzcVa8KJal.lrDU6xpS', 0, NULL, '2023-02-02 10:52:02', '0000-00-00 00:00:00');
+(1, '', 'genta@gmail.com', 'Admin', '$2y$10$hEGpc.Nl2hsWGlueaAUcZ.fmYDRAt1IKBB.myeFP.f5B4dBtHT2WG', 1, NULL, '2022-05-20 16:27:14', '0000-00-00 00:00:00'),
+(6152, '', 'genta@gmail.com', 'genta', '$2y$10$igkG68nfjHVy.MbL1kR3CecGXDOhvC5wg7NTOGbCpODfoVi1ILwf.', 1, NULL, '2022-11-21 04:16:03', NULL),
+(6157, '', 'genta@gmail.com', 'genta', '$2y$10$XmY4tlVxGNzRNb4utw3Hm.dyWu8m5hNfeRumRKV1LBdX.OH90nRzu', 0, NULL, '2024-06-03 11:45:25', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -229,31 +258,31 @@ ALTER TABLE `barangkeluar`
 -- AUTO_INCREMENT untuk tabel `barangmasuk`
 --
 ALTER TABLE `barangmasuk`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=131;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
 
 --
 -- AUTO_INCREMENT untuk tabel `databarang`
 --
 ALTER TABLE `databarang`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- AUTO_INCREMENT untuk tabel `datacustomer`
 --
 ALTER TABLE `datacustomer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `datasupplier`
 --
 ALTER TABLE `datasupplier`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6157;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6158;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
