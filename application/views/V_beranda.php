e24a4f9e092175d1656e1c1b2194f1c0cc7c12b0
WP2_Dimas.Maulana <163986448+Dimasmln01@users.noreply.github.com>
2024-06-24 22:06:00 +0700

    Update V_beranda.php
    fix eror

--- application/views/V_beranda.php
+++ application/views/V_beranda.php
@@ -28,11 +28,11 @@
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
+                            </div> 
+                            <div class="col-xl-3 col-md-6"> 
                                 <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Data Keluar</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <div class="card-body">Data Keluar</div> 
+                                    <div class="card-footer d-flex align-items-center justify-content-between"> 
                                         <!-- menuju laporan barangkeluar -->
                                         <a class="small text-white stretched-link" href="<?php echo base_url('Beranda/laporanbarangkeluar')?>">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
